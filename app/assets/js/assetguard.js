99a3ec818111b3a8a37c729ea23fc28a000a3401
SaGye9816 <default9811@gmail.com>
2021-12-14 01:36:42 +0900

    update

--- app/assets/js/assetguard.js
+++ app/assets/js/assetguard.js
@@ -296,7 +296,7 @@ class JavaGuard extends EventEmitter {
 
         const sanitizedOS = process.platform === 'win32' ? 'windows' : (process.platform === 'darwin' ? 'mac' : process.platform)
 
-        const url = 'https://api.adoptium.net/v3/assets/latest/${major}/hotspot?vendor=eclipse'
+        const url = 'https://api.adoptopenjdk.net/v2/latestAssets/nightly/openjdk${major}?os=${sanitizedOS}&arch=x64&heap_size=normal&openjdk_impl=hotspot&type=jre'
         
         return new Promise((resolve, reject) => {
             request({url, json: true}, (err, resp, body) => {
